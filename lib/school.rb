6268598c0f93ebb14b758dd3ce3db5ad8034d389
Irum F <irummf@gmail.com>
2018-09-01 17:10:31 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,26 +1,25 @@
 class School
-attr_accessor :name, :roster 
+  attr_accessor :name, :roster 
 
-def initialize (name)
-  @name = name 
-  @roster = {}
-end
+  def initialize (name)
+   @name = name 
+   @roster = {}
+  end
 
+  def add_student(name, grade)
+    roster[grade] ||= []  
+   roster[grade] << name
+  end 
 
-def add_student(name, grade)
-  roster[grade] ||= []  
-  roster[grade] << name
-end 
+  def grade(grade) 
+   roster[grade]
+  end 
 
-def grade(grade) 
-  roster[grade]
-end 
-
-def sort 
-  sorted = {}
-  roster.each do |grade, name|
+  def sort 
+    sorted = {}
+    roster.each do |grade, name|
     sorted[grade] = name.sort 
   end
-  sorted 
-end
+    sorted 
+  end 
 end
