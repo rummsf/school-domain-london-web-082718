59acffd74fc8613bbc255e5fc86b7acd42d029fa
Irum F <irummf@gmail.com>
2018-09-01 16:55:30 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,14 @@
-# code here!
+class School
+attr_accessor :name, :roster 
+
+def initialize (name)
+  @name = name 
+  @roster = {}
+end
+
+I thought that because it isa constant it needs to be in caps?
+
+def add_student(name, grade)
+  ROSTER << student 
+end 
+end 
